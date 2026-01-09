9c72011fa5abfda0e3297d3a33814a4f19fbf42b
ValioEilax <119453927+ValioEilax@users.noreply.github.com>
2026-01-09 19:05:43 +0200

    feat: add playRound function with  win/loss conditions and scoring

--- javascript.js
+++ javascript.js
@@ -21,5 +21,36 @@ function getHumanChoice() {
   return humanChoice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  let isPlayerWinner = false;
+
+  if (humanChoice === computerChoice) {
+    alert("It's a tie! Play new round");
+    return;
+  }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    isPlayerWinner = true;
+  }
+
+  if (isPlayerWinner) {
+    alert(
+      `You won! Your '${humanChoice}' wins computer's '${computerChoice}'. Play a new round`
+    );
+    humanScore += 1;
+  } else {
+    alert(
+      `You lost! Computer's '${computerChoice}' wins your '${humanChoice}'. Play a new round`
+    );
+    computerScore += 1;
+  }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+console.log(playRound(humanChoice, computerChoice));
